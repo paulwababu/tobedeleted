41e7e521f07e952912ca127b7a290692b6fdcff3
paulwababu <44939805+paulwababu@users.noreply.github.com>
2023-09-14 08:46:45 +0300

    Update config.js

--- editors/demo/src/AskAIEditor/config/config.js
+++ editors/demo/src/AskAIEditor/config/config.js
@@ -9,11 +9,62 @@ import {
 } from 'wax-prosemirror-services';
 import AskAiContentService from '../AiService/AskAiContentService';
 
-async function myDummyPromise() {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve('Hi from AI Service');
-    }, 4150);
+//async function myDummyPromise(userInput) {
+// return new Promise((resolve, reject) => {
+// setTimeout(() => {
+//  console.log('User input:', userInput);
+// if (userInput === 'reject') {
+//  reject('Your request could not be processed for now');
+//} else {
+//resolve(
+// 'He made significant contributions to theoretical physics, including achievements in quantum mechanics',
+//);
+//}
+//}, 4150);
+//});
+//}
+
+const API_KEY = '';
+
+function myLLMPromise(userInput) {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const response = await fetch(
+        'https://api.openai.com/v1/chat/completions',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${API_KEY}`,
+          },
+          body: JSON.stringify({
+            model: 'gpt-4',
+            messages: [
+              {
+                role: 'user',
+                content: userInput,
+              },
+            ],
+            temperature: 1,
+            max_tokens: 3096,
+            top_p: 1,
+            frequency_penalty: 0,
+            presence_penalty: 0,
+          }),
+        },
+      );
+
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data);
+        resolve(data.choices[0].message.content.trim());
+      } else {
+        reject('API request failed');
+      }
+    } catch (e) {
+      console.error(e);
+      reject('Your request could not be processed for now');
+    }
   });
 }
 
@@ -40,7 +91,7 @@ export default {
   ],
   SchemaService: DefaultSchema,
   AskAiContentService: {
-    AskAiContentTransformation: myDummyPromise,
+    AskAiContentTransformation: myLLMPromise,
   },
   services: [
     new InlineAnnotationsService(),
